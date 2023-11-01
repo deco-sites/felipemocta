936a84b3a08b79d2a153415659bba207330d0b55
ramon38 <ramon@fetch.ly>
2023-11-01 00:42:41 -0300

    felipe mogta

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,25 +2,27 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$0 from "./sections/Hero.tsx";
+import * as $$$$$$0 from "./sections/SimpleTextList.tsx";
 import * as $$$$$$1 from "./sections/Theme.tsx";
-import * as $$$$$$2 from "./sections/Footer.tsx";
-import * as $$$$$$3 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$2 from "./sections/FelipeMogta.tsx";
+import * as $$$$$$3 from "./sections/Footer.tsx";
 import * as $$$$$$4 from "./sections/ParagraphList.tsx";
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$5 from "./sections/Hero.tsx";
+import * as $$$$$$$$$$$0 from "./apps/site.ts";
+import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
 
 const manifest = {
   "sections": {
-    "deco-sites/felipemocta/sections/Footer.tsx": $$$$$$2,
-    "deco-sites/felipemocta/sections/Hero.tsx": $$$$$$0,
+    "deco-sites/felipemocta/sections/FelipeMogta.tsx": $$$$$$2,
+    "deco-sites/felipemocta/sections/Footer.tsx": $$$$$$3,
+    "deco-sites/felipemocta/sections/Hero.tsx": $$$$$$5,
     "deco-sites/felipemocta/sections/ParagraphList.tsx": $$$$$$4,
-    "deco-sites/felipemocta/sections/SimpleTextList.tsx": $$$$$$3,
+    "deco-sites/felipemocta/sections/SimpleTextList.tsx": $$$$$$0,
     "deco-sites/felipemocta/sections/Theme.tsx": $$$$$$1,
   },
   "apps": {
-    "deco-sites/felipemocta/apps/decohub.ts": $$$$$$$$$$$0,
-    "deco-sites/felipemocta/apps/site.ts": $$$$$$$$$$$1,
+    "deco-sites/felipemocta/apps/decohub.ts": $$$$$$$$$$$1,
+    "deco-sites/felipemocta/apps/site.ts": $$$$$$$$$$$0,
   },
   "name": "deco-sites/felipemocta",
   "baseUrl": import.meta.url,
