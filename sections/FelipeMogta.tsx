a71222513922e7bf17682e209db5982148194913
ramon38 <ramon@fetch.ly>
2023-11-01 00:52:20 -0300

    felipe mogta

--- sections/FelipeMogta.tsx
+++ sections/FelipeMogta.tsx
@@ -1,12 +1,13 @@
 export interface Props {
-  title: string;
+  title?: string;
 }
 
-export default function LatestPosts({ title }: Props) {
+export default function FelipeMogta({
+  title = "felypy moktah",
+}: Props) {
   return (
-    <div>
-      <h1 class="font-bold">{title}</h1>
-      <p>This is an example section</p>
+    <div class="lg:container mx-8 md:mx-16 lg:mx-auto mt-8 md:mt-12 mb-28 text-3xl md:text-base">
+      {title}
     </div>
   );
 }
